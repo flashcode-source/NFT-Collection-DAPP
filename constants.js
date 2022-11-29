acfd21d57bf6c4cf78d4d8994c132f6b7e8e37c9
Abdulrahman Muhammad <abdul.muhammad9ja@gmail.com>
2022-11-29 18:34:37 +0100

    My NFT Whitelist dapp

---
+++ constants.js
@@ -0,0 +1,75 @@
+// contract address
+export const CONTRACT_ADDRESS = "0x7d9C57731A14014a6c98FBEaF26b7B12741c56Ca";
+
+// abi
+export const ABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint8",
+          "name": "_maxNumber",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_address",
+          "type": "address"
+        }
+      ],
+      "name": "addAddressToWhitelist",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "maxWhitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "numAddressesWhitelisted",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "whitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
