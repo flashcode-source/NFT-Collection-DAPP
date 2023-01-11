29fab71d2d496a5f8e254f8f7f0927987995fd12
Abdulrahman Muhammad <abdul.muhammad9ja@gmail.com>
2023-01-11 15:29:50 +0100

    updated the image uri

--- pages/api/[tokenId].js
+++ pages/api/[tokenId].js
@@ -5,7 +5,7 @@ const handler = (req, res) => {
     const metaData = {
       name: `Crypto Devs #${tokenId}`,
       description: "It's an NFT for crypto developers",
-      image: `https://nft-collection-dapp-psi.vercel.app/api/nft/${tokenId}.svg`
+      image: `https://nft-collection-dapp-psi.vercel.app/nft/${tokenId}.svg`
     }
     res.status(200).json(metaData)
   }
